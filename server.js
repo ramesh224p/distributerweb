6751ab725f099daf1ff362777f859370758df008
ramesh224p <patilpatil577@gmail.com>
2021-04-04 12:05:17 +0530

    port chnage

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
